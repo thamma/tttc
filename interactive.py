201f123c7eca0616bfe4c3621a7533c06f698395
Dominic Zimmer <s8dozimm@stud.uni-saarland.de>
2019-08-08 09:55:49 +0200

    Remove relics of older versions

--- interactive.py
+++ interactive.py
@@ -1,12 +1,8 @@
 from telethon import sync, TelegramClient
+import tttcutils
 import os
 
-if not ("TTTC_API_ID" in os.environ or "TTTC_API_HASH" in os.environ):
-    print("Please set your environment variables \"TTTC_API_ID\" and \"TTTC_API_HASH\" accordingly.")
-    print("Please consult https://core.telegram.org/api/obtaining_api_id on how to get your own API id and hash.")
-    quit(1)
-api_id = os.environ["TTTC_API_ID"]
-api_hash = os.environ["TTTC_API_HASH"]
+api_id, api_hash = tttcutils.assert_environment()
 
 client = TelegramClient("tttc", api_id, api_hash)
 client.connect()
